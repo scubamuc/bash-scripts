146716a19b108ebe20e2492c57f55bbe1afe4d4d
scubamuc <54933878+scubamuc@users.noreply.github.com>
2024-06-22 16:19:17 +0200

    Update nc-restart-cron.sh

--- nc-restart-cron.sh
+++ nc-restart-cron.sh
@@ -1,5 +1,13 @@
   #!/bin/bash
-### Restart Nextcloud cron ###
+### Restart Nextcloud cron it it failed ###
+### create cronjob as root to run every 5 min ###
 
-sudo snap restart nextcloud.nextcloud-cron
+SERVICE=cron.php
+
+if (( $(ps -eo pid,etime,cmd | awk '$2 ~ /[0-9]+:[0-9]{2}:[0-9]{2}/' | grep $SERVICE | wc -l) > 0 )) ; then
+    snap restart nextcloud.nextcloud-cron
+fi
+
+
+### sudo snap restart nextcloud.nextcloud-cron ###
 
