1ec6a5f43fe9109df73f71a71305f8a5b938005f
scubamuc <54933878+scubamuc@users.noreply.github.com>
2025-05-02 10:55:59 +0200

    Update auto-contacts-export.sh

--- auto-contacts-export.sh
+++ auto-contacts-export.sh
@@ -5,11 +5,12 @@
 # Nextcloud snap automatic Adressbook export
 #################################################################################
              #  Nextcloud snap contacts export, Variables #
-NCTLD="cloud.mydomain.tld"
-NCUSER="username"
-NCUSERPWD="userpassword"
-NCADBNAME="addressbookname"
-NCADBLINK="https://$NCTLD/remote.php/dav/addressbooks/users/$NCUSER/$NCADBNAME/?export"
+NCTLD="cloud.mydomain.tld" # Nextcloud TLD
+NCUSER="username" # Username
+NCUSERPWD="userpassword" #Userpassword
+NCADBNAME="addressbookname" #Addressbook name
+NCADBLINK="https://$NCTLD/remote.php/dav/addressbooks/users/$NCUSER/$NCADBNAME/?export" 
+NCADBFULLLINK="https://cloud.example.com/remote.php/dav/addressbooks/users/username/contacts/?export"  #optional Addressbook link
 ##############################################################
     echo "Downloading from $NCADBLINK..."
     curl -L -J -O -u "$NCUSER:$NCUSERPWD" "$NCADBLINK" --create-dirs -o "./$(basename "$url")"
