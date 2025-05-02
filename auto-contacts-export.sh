b3b6b166cd08798c920eb551412b3c103e4fe78d
scubamuc <54933878+scubamuc@users.noreply.github.com>
2025-05-02 10:49:58 +0200

    Update auto-contacts-export.sh

--- auto-contacts-export.sh
+++ auto-contacts-export.sh
@@ -8,24 +8,11 @@
 NCTLD="cloud.mydomain.tld"
 NCUSER="username"
 NCUSERPWD="userpassword"
-NCADMIN="admin" 
-NCADMINPW="adminpassword"
-NCADMINMAIL="admin@mydomain.tld"
-RPROXYIP="192.168.2.xxx"
-NCLANGUAGE="en"
-NCLOCALE="en"
-NCREGION="GB"
+NCADBNAME="addressbookname"
+NCADBLINK="https://$NCTLD/remote.php/dav/addressbooks/users/$NCUSER/$NCADBNAME/?export"
 ##############################################################
-
-# List Addressbook URL's
-downloadLinks=(
-    "https://cloud.example.com/remote.php/dav/calendars/cooper.bin/persnlich/?export"
-    # add instance URL's
-)
-for downloadLink in "${downloadLinks[@]}"
-do
-    echo "Downloading from $downloadLink..."
-    curl -L -J -O -u "$NCUSER:$NCUSERPWD" "$downloadLink" --create-dirs -o "./$(basename "$url")"
+    echo "Downloading from $NCADBLINK..."
+    curl -L -J -O -u "$NCUSER:$NCUSERPWD" "$NCADBLINK" --create-dirs -o "./$(basename "$url")"
 done
 
 echo "Download abgeschlossen."
