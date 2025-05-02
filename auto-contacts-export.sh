70af7584d715c9d53e73e85c34c0f0766f4d37a6
scubamuc <54933878+scubamuc@users.noreply.github.com>
2025-05-02 10:43:02 +0200

    Update auto-contacts-export.sh

--- auto-contacts-export.sh
+++ auto-contacts-export.sh
@@ -1,25 +1,31 @@
-#Nextcloud snap automatic Adressbook export
+ #!/bin/bash
+#################################################################################
+# Auto-contacts-export  -scubamuc- https://scubamuc.github.io/                  #
+#################################################################################
+# Nextcloud snap automatic Adressbook export
+#################################################################################
+             #  Nextcloud snap contacts export, Variables #
+NCTLD="cloud.mydomain.tld"
+NCUSER="username"
+NCUSERPWD="userpassword"
+NCADMIN="admin" 
+NCADMINPW="adminpassword"
+NCADMINMAIL="admin@mydomain.tld"
+RPROXYIP="192.168.2.xxx"
+NCLANGUAGE="en"
+NCLOCALE="en"
+NCREGION="GB"
+##############################################################
 
-```
-#!/bin/bash
-
-# Benutzername, Passwort
-username="BENUTZERNAME"
-password="PASSWORD"
-
-# List URLs
+# List Addressbook URL's
 downloadLinks=(
     "https://cloud.example.com/remote.php/dav/calendars/cooper.bin/persnlich/?export"
     # add instance URL's
 )
 for downloadLink in "${downloadLinks[@]}"
 do
-    # Der `-J` Schalter sagt curl, den Dateinamen aus dem Content-Disposition Header zu verwenden
-    # Der `-O` Schalter speichert die Datei unter ihrem ursprünglichen Namen
-    # Die Option `-L` erlaubt curl, Weiterleitungen zu folgen
-    # Authentifizierungsdaten werden mit der Option `-u` übergeben
     echo "Downloading from $downloadLink..."
-    curl -L -J -O -u "$username:$password" "$downloadLink" --create-dirs -o "./$(basename "$url")"
+    curl -L -J -O -u "$NCUSER:$NCUSERPWD" "$downloadLink" --create-dirs -o "./$(basename "$url")"
 done
 
 echo "Download abgeschlossen."
