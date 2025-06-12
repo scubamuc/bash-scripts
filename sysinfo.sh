d924d3a5fee93eb57a87cfa5895ed45ed773c8c4
scubamuc <54933878+scubamuc@users.noreply.github.com>
2025-06-12 21:49:09 +0200

    Update sysinfo.sh

--- sysinfo.sh
+++ sysinfo.sh
@@ -80,8 +80,7 @@ echo ''
         echo -ne "	    X11VNC Server: "; ps -C x11vnc >/dev/null && echo "läuft!" || echo "gestoppt!"
 	echo -ne "	System Läuft seit: "; uptime -p ; #check wie lange läuft System schon
 	echo -ne "	 Letzter Neustart: "; last reboot -F | head -1 | awk '{print $5,$6,$7,$8,$9}' ; #check letzter neustart
-	echo -ne "	  System Neustart? "; pro system reboot-required | grep 'no' >/dev/null && echo "nicht erforderlich!" || echo "ist erforderlich!" ; #prüfe nach ob reboot erforderlich ist
-echo ''
+	echo -ne "	  System Neustart? "; $CheckReboot
 echo ''
 echo '   =========================================   '
 echo ''
