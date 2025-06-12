cb14dabf0743949cdcc7a44fb7a6d4ae36d25c3a
scubamuc <54933878+scubamuc@users.noreply.github.com>
2025-06-12 21:51:35 +0200

    Update sysinfo_en.sh

--- sysinfo_en.sh
+++ sysinfo_en.sh
@@ -78,8 +78,8 @@ echo '       System services and messages!             '
 echo ''
 	echo -ne "      |   System up since:	"; uptime -p ; #check uptime
         echo -ne "      |         Last boot:	"; last reboot -F | head -1 | awk '{print $5,$6,$7,$8,$9}' ; # Check last reboot
-        echo     "      |    System updates?	" $CheckUpdates ; # check for system upgrades
-        echo -ne "      |   Reboot required?	" $CheckReboot ; # check if reboot is required
+        echo     "      |    System updates?	" CheckUpdates ; # check for system upgrades
+        echo -ne "      |   Reboot required?	" CheckReboot ; # check if reboot is required
 echo ''
 echo ''
 echo '       ==================================   '
